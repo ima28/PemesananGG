98bf3f2a40f363b7ac09a6191b944e633e7b37fe
ima28 <zumaroimaa@gmail.com>
2020-09-11 13:41:30 +0700

    yang muncul hanya btnnav,splash dan login tidak muncul

--- app/src/main/java/com/example/pemesanangg/Activity/Home.java
+++ app/src/main/java/com/example/pemesanangg/Activity/Home.java
@@ -13,44 +13,6 @@ import com.example.pemesanangg.Fragment.OrderFragment;
 import com.example.pemesanangg.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Home extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class Home extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        loadFragment(new HomeFragment());
-        setContentView(R.layout.activity_home);
-
-        loadFragment(new HomeFragment());
-        BottomNavigationView bottomNavigationView = findViewById(R.id.btnnav);
-        bottomNavigationView.setOnNavigationItemSelectedListener(this);
-    }
-
-    private boolean loadFragment(Fragment fragment) {
-        if (fragment != null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fl_container, fragment)
-                    .commit();
-            return true;
-        }
-        return false;
-
-    };
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        Fragment fragment = null;
-        switch (item.getItemId()) {
-            case R.id.home_menu:
-                fragment = new HomeFragment();
-                break;
-            case R.id.order_menu:
-                fragment = new OrderFragment();
-                break;
-            case R.id.lain_menu:
-                fragment = new LainFragment();
-                break;
-        }
-        return loadFragment(fragment);
-    };
 }
--- app/src/main/java/com/example/pemesanangg/Activity/MainActivity.java
+++ app/src/main/java/com/example/pemesanangg/Activity/MainActivity.java
@@ -1,19 +1,56 @@
 package com.example.pemesanangg.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.pemesanangg.Fragment.HomeFragment;
+import com.example.pemesanangg.Fragment.LainFragment;
+import com.example.pemesanangg.Fragment.OrderFragment;
 import com.example.pemesanangg.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-            Intent login=new Intent(MainActivity.this, Splashscreen.class);
-            startActivity(login);
+
+        getFragmentPage(new HomeFragment());
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.home_menu:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.order_menu:
+                        fragment = new OrderFragment();
+                        break;
+                    case R.id.lain_menu:
+                        fragment = new LainFragment();
+                        break;
+                }
+                return getFragmentPage(fragment);
+            }
+        });
+    }
+
+    private boolean getFragmentPage(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
